b0ad30ab9efbc9e4a21faf7dc72e2793dd551c1e
Rémy Sanchez <remy.sanchez@hyperthese.net>
2020-04-17 14:07:24 +0200

    Provide a "safe" proxy to httpx's model module

--- src/typefit/__init__.py
+++ src/typefit/__init__.py
@@ -2,3 +2,8 @@ from .fitting import Fitter, T, typefit
 from .meta import meta, other_field
 from .reporting import LogErrorReporter, PrettyJson5Formatter
 from .serialize import serialize
+
+try:
+    from httpx import _models as httpx_models
+except ImportError:
+    from httpx import models as httpx_models
