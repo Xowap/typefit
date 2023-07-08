eb87b40421a234060b9b2ff972368248b883704d
Rémy Sanchez <remy.sanchez@hyperthese.net>
2023-07-08 18:38:38 +0200

    #49 Making those comparisons more inclusive

---
+++ tests/issue_000049/test_broader_types.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Sequence, Mapping
+
+from typefit import typefit
+
+
+@dataclass
+class Something:
+    seq: Sequence[int]
+    map: Mapping[str, int]
+
+
+def test_broader_types():
+    x: Something = typefit(
+        Something,
+        dict(
+            seq=[1, 2, 3],
+            map=dict(a=1, b=2, c=3),
+        ),
+    )
+
+    assert x.seq == [1, 2, 3]
+    assert x.map == dict(a=1, b=2, c=3)
